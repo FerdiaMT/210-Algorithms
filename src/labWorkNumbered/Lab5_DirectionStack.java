d70cf51fe07e40210e3da3164d1c5045162e5c69
FerdiaMT <oscadot@gmail.com>
2024-11-05 23:32:01 +0000

    Added stack class

--- src/labWorkNumbered/Lab5_DirectionStack.java
+++ src/labWorkNumbered/Lab5_DirectionStack.java
@@ -1,7 +1,30 @@
 package labWorkNumbered;
 
+import java.util.Scanner;
+
 public class Lab5_DirectionStack {
 	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		
+		Lab5_Stack stack = new Lab5_Stack(10); // initializes stack with size 10
+		String input = "";
+		do {
+			input = sc.nextLine();
+			stack.push(input);
+		}
+		while(input !="Arrived");
+		
+		System.out.println("A");
 		
 	}
 }
+	
+	
+
+//inputs 
+//Go North
+//Go South
+//Go East
+//Go West
+//Go Back
+//Arrived
---
+++ src/labWorkNumbered/Lab5_Stack.java
@@ -0,0 +1,33 @@
+package labWorkNumbered;
+
+public class Lab5_Stack {
+
+	private int maxSize; // size of stack array
+	private String[] stackArray;
+	private int top; // top of stack
+	 
+	 
+	public Lab5_Stack(int n) {
+		maxSize = n; // set array size
+		stackArray = new String[maxSize]; // create array
+		top = -1; 
+	}
+	
+	public void push(String j) {
+		top++;
+		stackArray[top] = j;
+		// increment top, insert item
+	}
+	
+	public String pop() {
+		return stackArray[top--];
+		//access item, decrement top
+	} 
+	
+	public String peek() {
+
+		return stackArray[top];
+	} 
+	
+	
+}
