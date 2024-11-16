92354975708d39f3b324b8adf4bdd5169c71a76d
FerdiaMT <oscadot@gmail.com>
2024-11-16 14:52:52 +0000

    PriorityQueue Added

---
+++ src/_AlgorithmsFolder/_Stacks/PriorityQueue.java
@@ -0,0 +1,54 @@
+package _AlgorithmsFolder._Stacks;
+
+public class PriorityQueue {
+
+	private int[] array;
+	private int maxSize;
+	private int nItems;
+	private int front;
+	private int rear;
+
+	
+	public PriorityQueue(int size) {
+		maxSize = size;
+		array = new int[maxSize];
+		nItems = 0;
+		front = 0;
+		rear = -1;
+	}
+	
+	public void insert(int input) {
+
+		if(nItems ==0) { // if empty, make array 0 the input;
+			array[0] = input; // an array of 1 is sorted
+		}else {
+			int j = nItems; // j is acting as a pointer that starts from the current amount of items
+			
+			while(j>0 && array[j-1] > input) { // while j is bigger than 0 and  the value below is bigger then our input
+				array[j] = array[j-1];
+				j--;
+			}
+			
+			array[j] = input;
+		}
+		
+		
+		nItems++;
+		
+	}
+	
+	public int remove() {
+		if(isFull())	return (Integer) null;
+		
+		int temp = array[front];
+		if(front == maxSize-1) {
+			front = -1;
+		}
+		front++;
+		return temp;
+	}
+	
+	public boolean isFull() {
+		return nItems == maxSize;
+	}
+}
---
+++ src/_AlgorithmsFolder/_Stacks/PriorityQueueHandler.java
@@ -0,0 +1,23 @@
+package _AlgorithmsFolder._Stacks;
+
+public class PriorityQueueHandler {
+
+	public static void main(String[] args) {
+		int size = 10;
+		PriorityQueue pq = new PriorityQueue(size);
+		
+		pq.insert(10);
+		pq.insert(15);
+		pq.insert(3);
+		pq.insert(22);
+		pq.insert(1);
+		
+		
+		System.out.println(pq.remove());
+		System.out.println(pq.remove());
+		System.out.println(pq.remove());
+		System.out.println(pq.remove());
+		System.out.println(pq.remove());
+		
+	}
+}
