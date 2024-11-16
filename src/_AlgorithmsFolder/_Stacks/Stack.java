e62196429fbf0d878d2bda17b26c39c4ec69af30
FerdiaMT <oscadot@gmail.com>
2024-11-16 14:00:35 +0000

    Added the stack classes for revision

---
+++ src/_AlgorithmsFolder/_Stacks/Stack.java
@@ -0,0 +1,57 @@
+package _AlgorithmsFolder._Stacks;
+
+public class Stack {
+
+	private int maxSize;
+	private int[] array;
+	private int top;
+	
+	
+	public Stack(int maxSize) {
+		this.maxSize = maxSize;
+		array = new int[this.maxSize];
+		top = -1;
+	}
+	
+	//push , pop , peek
+	public void push(int input) {
+		top++;
+		array[top] = input;
+	}
+	
+	public int pop() {
+		return array[top--];
+	}
+	
+	public int peek() {
+		return array[top];
+	}
+	
+	// ==============
+	// 	THE BOOLEANS 
+	// 		
+	//	  isFull
+	//	  isEmpty
+	//	 makeEmpty
+	//
+	// ==============
+	
+	public boolean isFull() { // pointer is at top of array, since array maxSize makes indexes all the way up to maxSize-1, we compare to maxSize -1
+		return top==maxSize-1;
+	}
+	
+	public boolean isEmpty() {//pointers at -1, since 0 is the first index;
+		return top==-1;
+	}
+	
+	public void makeEmpty() {
+		top=-1;
+	}
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ src/_AlgorithmsFolder/_Stacks/StackHandler.java
@@ -0,0 +1,18 @@
+package _AlgorithmsFolder._Stacks;
+
+public class StackHandler {
+	public static void main(String[] args) {
+		int stackSize = 10;
+		Stack stack = new Stack(stackSize);
+		stack.push(10);
+		stack.push(11);
+		stack.push(7);
+		stack.push(10);
+		stack.push(5);
+		
+		
+		while(stack.isEmpty()==false) {
+			System.out.println(stack.pop());
+		}
+	}
+}
