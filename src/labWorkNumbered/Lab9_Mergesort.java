bdfb22b69d136dd6be7e16ef608b34dd83f62c00
FerdiaMT <oscadot@gmail.com>
2024-12-07 23:42:54 +0000

    redoing mergesort

--- src/labWorkNumbered/Lab9_Mergesort.java
+++ src/labWorkNumbered/Lab9_Mergesort.java
@@ -9,50 +9,24 @@ public class Lab9_Mergesort {
 //		merge( workspace,first,second,upperBound);
 //	}
 //	
-	static void merge(int[] arr, int lowerBound , int upperBound, int middle) {
-		
-		int n1 = middle - lowerBound+1;
-		int n2 = upperBound-middle;
-		
-		int left [] = new int[n1];
-		int right[] = new int[n2];
-		
-		for(int i = 0 ; i < n1; i++) {
-			left[i] = arr[lowerBound+i];
-		}
-		for(int j = 0 ; j < n2; j++) {
-			right[j] = arr[middle+1+j];
-		}
-		
-		int i = 0 ; int j = 0;
-		int k = lowerBound;
-		
-		while (i < n1 && j < n2) {
-            if (left[i] <= right[j]) {
-                arr[k] = left[i];
-                i++;
-            }
-            else {
-                arr[k] = right[j];
-                j++;
-            }
-            k++;
-        }
-
-        // Copy remaining elements of L[] if any
-        while (i < n1) {
-            arr[k] = left[i];
-            i++;
-            k++;
-        }
-
-        // Copy remaining elements of R[] if any
-        while (j < n2) {
-            arr[k] = right[j];
-            j++;
-            k++;
-        }
-	}
+	public void merge(long[] workSpace, int first, int second, int upperBound) {
+			 int j = 0; // workspace index
+			 int lowerBound = first;
+			 int mid = second - 1;
+			 int n = upperBound-lowerBound+1; // # of items
+			 while(first <= mid && second <= upperBound) //halves not empty
+			 if( theArray[first] < theArray[second] )
+			 workSpace[j++] = theArray[first++];
+			 else
+			 workSpace[j++] = theArray[second++];
+			 while(first <= mid) //check first half for remaining
+			 workSpace[j++] = theArray[first++];
+			 while(second <= upperBound) //check second half for remaining
+			 workSpace[j++] = theArray[second++];
+			 for(j=0; j<n; j++)
+			 theArray[lowerBound+j] = workSpace[j]; //copy the workspace
+			back
+			} // end merge()
 	
 	public static void main(String[] args) {
 		int arr[] = {1,4,3,8,6,6,7,272772,727,8,956,84,73,44,2,523525,5,2522,6,78,5,93};
