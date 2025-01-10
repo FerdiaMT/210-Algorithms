0d426674f1e03a07ce7a313e929b7887d469bec6
FerdiaMT <oscadot@gmail.com>
2025-01-10 01:07:26 +0000

    More scraps to be added

--- src/_AlgorithmsFolder/scrapWork/PartA.java
+++ src/_AlgorithmsFolder/scrapWork/PartA.java
@@ -7,6 +7,35 @@ public class PartA {
 		
 		int[] arr = {2,5,3,4,1,0};
 		
+		//bubble sort
+		// goes from
+		
+		//insertion sort
+		
+		
+//		for(int i = 1; i < arr.length; i++) {
+//			
+//			int temp = arr[i];
+//			int j = i-1;
+//			
+//			while(j>=0 && arr[j]>temp) {
+//				arr[j+1]= arr[j];
+//				j--;
+//			}
+//			arr[j+1] = temp;
+//		}
+			
+		
+		
+		
+		for(int i = 0 ; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+		
+		
+		
+		
+		
 		//today we are going to code 3 algorithms
 		
 		///bubble sort
